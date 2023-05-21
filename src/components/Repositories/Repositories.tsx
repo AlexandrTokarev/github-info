0e3e4c137731bb9f1cdc0e26e62f011a24a260d1
Alexandr Tokarev <a.tokarev@dats.team>
2023-05-21 15:47:43 +0600

    Fix repo list

--- src/components/Repositories/Repositories.tsx
+++ src/components/Repositories/Repositories.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { Accordion, Spinner } from 'react-bootstrap';
 
 import { useGitHub } from '@/hooks/useGitHub';
@@ -6,16 +6,20 @@ import { useGitHub } from '@/hooks/useGitHub';
 const Repositories: FC = () => {
   const { user, repos } = useGitHub();
 
+  const list = useMemo(() => {
+    return repos.list.filter((repo) => repo.name !== user?.login);
+  }, [repos.list, user?.login]);
+
   if (!user) {
     return null;
   }
 
   return (
     <>
-      <h5>Public repositories</h5>
+      <h5>Public repositories: {list.length}</h5>
       {repos.isLoading && <Spinner animation="border" />}
       <Accordion>
-        {repos.list.map((repo) => (
+        {list.map((repo) => (
           <Accordion.Item key={repo.id} eventKey={String(repo.id)}>
             <Accordion.Header>{repo.name}</Accordion.Header>
             <Accordion.Body>Body</Accordion.Body>
